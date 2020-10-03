a90e8a1a9b60ee6cdd04f36dd1dc839a5e0ef1c3
Mahendra Sutar <mahendrasutar8.ms@gmil.com>
2020-10-03 22:25:06 +0530

    Updates

---
+++ precache-manifest.7659500624fd833f88b8d0807ebe8340.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e23f1f6804c9ba12aebfacf7973184",
+    "url": "/moviedata/index.html"
+  },
+  {
+    "revision": "8d902738719daba15e56",
+    "url": "/moviedata/static/css/main.f4c7c797.chunk.css"
+  },
+  {
+    "revision": "68af0af48077bbd96314",
+    "url": "/moviedata/static/js/2.ea984c05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedata/static/js/2.ea984c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d902738719daba15e56",
+    "url": "/moviedata/static/js/main.a2060862.chunk.js"
+  },
+  {
+    "revision": "1b8a111d9fc4e37c3253",
+    "url": "/moviedata/static/js/runtime-main.39c61cf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moviedata/static/media/logo.5d5d9eef.svg"
+  }
+]);
